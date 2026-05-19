a10ccdc7f2d92d230119f671c819c9d917d31656
Bruno Donadei <bruno.donadei@hiplatform.com>
2026-05-18 22:47:23 -0300

    refactor: optimize student listing queries

--- src/main/java/com/gymiq/repository/StudentRepository.java
+++ src/main/java/com/gymiq/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package com.gymiq.repository;
 import com.gymiq.entity.Student;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,14 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    @Override
+    @EntityGraph(attributePaths = "user")
+    Page<Student> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = "user")
+    Optional<Student> findById(Integer id);
 
     Optional<Student> findByCpf(String cpf);
 
@@ -23,6 +32,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
             "LOWER(u.name) LIKE LOWER(CONCAT('%', :term, '%')) OR " +
             "s.cpf LIKE CONCAT('%', :term, '%') OR " +
             "LOWER(u.email) LIKE LOWER(CONCAT('%', :term, '%'))")
+    @EntityGraph(attributePaths = "user")
     Page<Student> searchByTerm(@Param("term") String term, Pageable pageable);
 
 }
