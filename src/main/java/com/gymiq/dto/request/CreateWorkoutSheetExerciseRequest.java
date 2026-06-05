53d94082c038ec8702b3bee896f8ffde81ebbb58
Gustavo Soares <11222101537@alunos.umc.br>
2026-06-05 20:42:10 -0300

    Adiciona limites para exercicios da ficha

--- src/main/java/com/gymiq/dto/request/CreateWorkoutSheetExerciseRequest.java
+++ src/main/java/com/gymiq/dto/request/CreateWorkoutSheetExerciseRequest.java
@@ -7,10 +7,12 @@ import lombok.Data;
 public class CreateWorkoutSheetExerciseRequest {
 
     @NotNull(message = "ID do exercicio e obrigatorio")
+    @Min(value = 1, message = "ID do exercicio deve ser maior que zero")
     private Integer exerciseId;
 
     @NotNull(message = "Series e obrigatorio")
     @Min(value = 1, message = "Series deve ser maior que zero")
+    @Max(value = 100, message = "Series deve ser no maximo 100")
     private Integer sets;
 
     @NotBlank(message = "Repeticoes e obrigatorio")
@@ -18,6 +20,7 @@ public class CreateWorkoutSheetExerciseRequest {
     private String repetitions;
 
     @Min(value = 0, message = "Descanso nao pode ser negativo")
+    @Max(value = 1200, message = "Descanso deve ser no maximo 1200 segundos")
     private Integer restSeconds;
 
     @Size(max = 40, message = "Treino deve ter no maximo 40 caracteres")
@@ -25,6 +28,7 @@ public class CreateWorkoutSheetExerciseRequest {
 
     @NotNull(message = "Ordem de execucao e obrigatoria")
     @Min(value = 1, message = "Ordem de execucao deve ser maior que zero")
+    @Max(value = 100, message = "Ordem de execucao deve ser no maximo 100")
     private Integer executionOrder;
 
     @Size(max = 255, message = "Observacoes devem ter no maximo 255 caracteres")
